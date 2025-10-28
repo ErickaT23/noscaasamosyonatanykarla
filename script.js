8f02bfeb71e3c885db2c8918a941997f8fc346ea
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-10-27 20:50:40 -0600

    first commit

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", function() {
         togglePlayPause();
     });
 
-    const targetDate = new Date('2025-09-20T00:00:00').getTime();
+    const targetDate = new Date('2026-01-03T00:00:00').getTime();
     const countdown = setInterval(() => {
         const now = new Date().getTime();
         const distance = targetDate - now;
